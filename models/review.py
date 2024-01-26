bc4c4e8ccdb04ae359e8e06af3dd5c98138cc822
Null <kelanidarasimi9@gmail.com>
2024-01-26 17:02:57 +0100

    crete the review table add the declarative_base to inherit from

--- models/review.py
+++ models/review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'reviews'
