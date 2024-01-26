f122c2e6e7e52211c627f46e064b18e31849eb93
Null <kelanidarasimi9@gmail.com>
2024-01-26 11:19:17 +0100

    setup db storage
    create and setup the database then I creaated the states and cities table

---
+++ models/engine/db_storage.py
@@ -0,0 +1,75 @@
+"""
+The database storage engine setup
+"""
+from os import getenv
+from models.base_model import Base
+from sqlalchemy import MetaData, create_engine
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
+
+# the connection string setup for connecting mysql to sqlalchemy
+CONNECTION_STRING = "mysql+mysqldb://{}:{}@{}/{}?charset=utf8mb4".\
+    format(getenv('HBNB_MYSQL_USER'), getenv('HBNB_MYSQL_PWD'),
+           getenv('HBNB_MYSQL_HOST'), getenv('HBNB_MYSQL_DB'))
+
+class DBStorage:
+    """An object that handles setting up the database"""
+    __engine = None
+    __session = None
+    
+    def __init__(self):
+        self.__engine = create_engine(CONNECTION_STRING, pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(bind=self.__engine)
+        
+    def all(self, cls=None):
+        """ Query all objects from the current database session """
+        from models import storage
+        classes = {"State": State, "Amenity": Amenity, "City": City,
+                   "User": User, "Place": Place, "Review": Review
+                   }
+        result = {}
+        if cls:
+            if isinstance(cls, str):
+                cls = classes[cls]
+            if cls in classes.values():
+                objects = self.__session.query(cls).all()
+                for obj in objects:
+                    key = f'{type(obj).__name__}.{obj.id}'
+                    result[key] = obj
+                return result
+        else:
+            for c in classes.values():
+                objects = self.__session.query(c).all()
+                for obj in objects:
+                    key = f'{type(obj).__name__}.{obj.id}'
+                    result[key] = obj
+            return result
+ 
+    def new(self, obj):
+        """add instance to current database session"""
+        self.__session.add(obj)
+    
+    def save(self):
+        """ save current session to database"""
+        self.__session.commit()
+        
+    def delete(self, obj=None):
+        """
+        delete data from database
+        """
+        if obj:
+            self.__session.delete(obj)
+        
+    def reload(self):
+        """Reload database"""
+        from sqlalchemy.orm import sessionmaker, scoped_session
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+storage_type = getenv('HBNB_TYPE_STORAGE')
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    if storage_type == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        state = relationship('City', backref='state', cascade='all, delete-orphan')
+
+    #Filestorage
+    else:
+        name = ""
+        @property
+        def cities(self):
+            #TODO: set a getter attribute that returns a list of cities instance
+            # state_id
+            return self
