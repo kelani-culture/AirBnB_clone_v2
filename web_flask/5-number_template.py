e68441a83eb7a1f401c269ce5b553dd828928d06
Null <kelanidarasimi9@gmail.com>
2024-01-27 22:15:18 +0100

    run a simple flask app 6 redid

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -23,8 +23,8 @@ def c_is_fun(text):
 
 
 @app.route('/python/<text>', strict_slashes=False)
-@app.route('/python', strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
+# @app.route('/python', strict_slashes=False)
+# @app.route('/python/', strict_slashes=False)
 def python_is_cool(text=None):
     if text:
         text = text.replace('_', ' ')
